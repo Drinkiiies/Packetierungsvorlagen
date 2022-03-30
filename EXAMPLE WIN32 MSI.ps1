e3d3e56dae96a02dddca61dd2d288891613b6160
Drinkiiies <102231005+Drinkiiies@users.noreply.github.com>
2022-03-30 09:57:20 +0200

    Update EXAMPLE WIN32 MSI.ps1

--- EXAMPLE WIN32 MSI.ps1
+++ EXAMPLE WIN32 MSI.ps1
@@ -56,6 +56,6 @@
 #START PROGRAM
 
 #Wait ZWINGEND nötig. Alternativ pipe auf out-null
-(Start-Process -Wait "$($PSScriptRoot)\30B712C\setup.msi" -ArgumentList "/qn").ExitCode
+(Start-Process -Wait "$($PSScriptRoot)\setup.msi" -ArgumentList "/qn").ExitCode
 
 #END PROGRAM
