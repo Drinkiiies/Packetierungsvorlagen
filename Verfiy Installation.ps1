202545324bf5f9d8ee22632c550a3982c2f94584
Drinkiiies <102231005+Drinkiiies@users.noreply.github.com>
2022-04-21 14:20:21 +0200

    Add files via upload

---
+++ Verfiy Installation.ps1
@@ -0,0 +1,9 @@
+﻿#Zu suchendes Programm. Achtung: CASE SENSITIVE
+$Name = "Printix"
+$result = (Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | findstr.exe $Name)
+
+if ($result.Length -ge "1" ){
+Write-Host "exit 0"
+}else{
+Write-Host "exit 1"
+}
